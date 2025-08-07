ac6582f00179454ea715cda733eb218824cdaa1d
Abhinav Tyagi <Abhinavtyagiup@gmail.com>
2025-08-07 22:18:55 +0530

    header ui update

--- components/header.jsx
+++ components/header.jsx
@@ -28,15 +28,23 @@ const Header = async () => {
         {/* Navigation Links - Different for signed in/out users */}
         <div className="hidden md:flex items-center space-x-8">
           <SignedOut>
-            <a href="#features" className="text-gray-600 hover:text-blue-600">
-              Features
-            </a>
-            <a
-              href="#testimonials"
-              className="text-gray-600 hover:text-blue-600"
-            >
-              Testimonials
-            </a>
+             <div className="flex space-x-4 bg-dark-700 p-2 rounded-md">
+  <a
+    href="#features"
+    className="px-4 py-2 rounded-md text-sm font-medium text-white bg-blue-500"
+  >
+    Features
+  </a>
+
+  <a
+    href="#testimonials"
+    className="px-4 py-2 rounded-md text-sm font-medium text-white bg-blue-500"
+
+  >
+    Testimonials
+  </a>
+</div>
+   
           </SignedOut>
         </div>
 
